4b43db7a10814cd45b67897ce04482fc0edf744e
Gabriele <cristiine94@outlook.com>
2023-05-29 17:34:21 -0300

    Organizando Repositorio

--- src/VetoresEMatrizes/matrizesExc2.java
+++ src/VetoresEMatrizes/matrizesExc2.java
@@ -4,38 +4,40 @@ public class matrizesExc2 {
 
 	public static void main(String[] args) {
      
-	double[][] notas = new double[10][4];
-      double[] medias = new double[10];
-      Scanner scanner = new Scanner(System.in);
+		  double[][] notas = new double[10][4];
+	        double[] medias = new double[10];
+	        Scanner scanner = new Scanner(System.in);
 
-      // Leitura das notas dos participantes
-      for (int i = 0; i < notas.length; i++) {
-          System.out.println("Digite as notas do participante " + (i + 1) + ":");
-          for (int j = 0; j < notas[i].length; j++) {
-              System.out.print("Nota " + (j + 1) + ": ");
-              notas[i][j] = scanner.nextDouble();
-          }
-          System.out.println();
-      }
+	        // Leitura das notas dos participantes
+	        for (int i = 0; i < notas.length; i++) {
+	            System.out.println("Digite as notas do participante " + (i + 1) + ":");
+	            for (int j = 0; j < notas[i].length; j++) {
+	                System.out.print("Nota do bimestre " + (j + 1) + ": ");
+	                notas[i][j] = scanner.nextDouble();
+	            }
+	            System.out.println();
+	        }
 
-      // Cálculo das médias dos participantes
-      for (int i = 0; i < notas.length; i++) {
-          double soma = 0;
-          for (int j = 0; j < notas[i].length; j++) {
-              soma += notas[i][j];
-          }
-          medias[i] = soma / notas[i].length;
-      }
+	        // Cálculo das médias dos participantes
+	        for (int i = 0; i < notas.length; i++) {
+	            double soma = 0;
+	            for (int j = 0; j < notas[i].length; j++) {
+	                soma += notas[i][j];
+	            }
+	            medias[i] = soma / notas[i].length;
+	        }
 
-      // Exibição das médias dos participantes
-      System.out.println("Médias dos participantes:");
-      for (int i = 0; i < medias.length; i++) {
-          System.out.println("Participante " + (i + 1) + ": " + medias[i]);
-      }
+	        // Exibição das médias dos participantes
+	        System.out.println("Médias dos participantes:");
+	        for (int i = 0; i < medias.length; i++) {
+	            System.out.println("Participante " + (i + 1) + ": " + medias[i]);
+	        }
 
-      scanner.close();
-  }
-}
+	        scanner.close();
+	    }
+	}
+
+  
 		
 	
 
